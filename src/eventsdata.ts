7051da0c6efa03d1f5834dc0a92d65e16d02b0b7
James Prickitt <james@precision-websites.com>
2024-05-13 21:43:11 -0400

    Update eventsdata.ts

--- src/eventsdata.ts
+++ src/eventsdata.ts
@@ -1,131 +1,131 @@
 export const eventsDataJSON = [
   {
     start: {
-      dateTime: "2024-04-06T08:30:00-04:00",
+      dateTime: "2024-05-13T08:30:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T09:19:00-04:00",
+      dateTime: "2024-05-13T09:19:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T10:30:00-04:00",
+      dateTime: "2024-05-13T10:30:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T10:58:00-04:00",
+      dateTime: "2024-05-13T10:58:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-08T12:00:00-04:00",
+      dateTime: "2024-05-15T12:00:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-08T13:00:00-04:00",
+      dateTime: "2024-05-15T13:00:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-09T12:00:00-04:00",
+      dateTime: "2024-05-16T12:00:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-09T13:00:00-04:00",
+      dateTime: "2024-05-16T13:00:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-07T14:00:00-04:00",
+      dateTime: "2024-05-15T14:00:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-07T14:20:00-04:00",
+      dateTime: "2024-05-15T14:20:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-07T16:45:00-04:00",
+      dateTime: "2024-05-15T16:45:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-07T17:30:00-04:00",
+      dateTime: "2024-05-15T17:30:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T16:30:00-04:00",
+      dateTime: "2024-05-13T16:30:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T17:15:00-04:00",
+      dateTime: "2024-05-13T17:15:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T18:30:00-04:00",
+      dateTime: "2024-05-13T18:30:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T19:30:00-04:00",
+      dateTime: "2024-05-13T19:30:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-08T18:40:00-04:00",
+      dateTime: "2024-05-15T18:40:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-08T18:55:00-04:00",
+      dateTime: "2024-05-15T18:55:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T21:20:00-04:00",
+      dateTime: "2024-05-13T21:20:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T21:50:00-04:00",
+      dateTime: "2024-05-13T21:50:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T21:30:00-04:00",
+      dateTime: "2024-05-13T21:30:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T21:40:00-04:00",
+      dateTime: "2024-05-13T21:40:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T22:00:00-04:00",
+      dateTime: "2024-05-13T22:00:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T22:10:00-04:00",
+      dateTime: "2024-05-13T22:10:00-04:00",
       timeZone: "America/New_York",
     },
   },
   {
     start: {
-      dateTime: "2024-04-06T22:50:00-04:00",
+      dateTime: "2024-05-13T22:50:00-04:00",
       timeZone: "America/New_York",
     },
     end: {
-      dateTime: "2024-04-06T23:10:00-04:00",
+      dateTime: "2024-05-13T23:10:00-04:00",
       timeZone: "America/New_York",
     },
   },
